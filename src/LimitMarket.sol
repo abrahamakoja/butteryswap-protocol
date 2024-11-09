a0dfc9d9ae17f626f597eb1428e8637f76cb4d27
abrahamakoja <abrahamakoja25@gmail.com>
2024-11-09 18:11:11 +0100

    latest

--- src/LimitMarket.sol
+++ src/LimitMarket.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.20;
 // Imports
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
 import {CreateMultiSig} from "./CreateMultiSig.sol";
 
 // Errors
@@ -15,20 +17,24 @@ error inValidAmount(uint256 balance, uint256 amountsent);
 error NoAmountSent(uint256 balance, uint256 amountsent);
 
 // Type Declarations
-struct MultiSigDetails {
+struct borrowRequest {
     uint256 depositedAmount;
     uint256 collateral;
+ uint256 position;
+    // add balance of tokens, address and any value to be used
 }
 
 // Contract Definition
-contract LimitMarket {
+contract LimitMarket is ReentrancyGuard{
     using SafeERC20 for IERC20;
 
     // State Variables
-    mapping(address => address[]) public userToMultiSigs; // Mapping for all multisigs created by users
-    mapping(address => MultiSigDetails) public multiSigDetails; // Struct holding multisig details
-    mapping(address => address[]) public borrowRequests; // Supports multiple multisig requests per contract
-
+    uint256 public totalBorrowersCount;
+    address[] public protol_TotalActiveBorrowRequest_Array;//total active borrow requests contract address in the protocol
+    address[] public protocolUsers;// list of all wallets interacting with buttery swap protocol || may move this smwhere else
+    mapping(address => borrowRequest) public userToBorrowRequestDetails; //stores a mapping of a specific user wallet address to the struct borrow request.
+    mapping(address => address[]) public userToBorrowRequestAddress; //stores a mapping of a specific user wallet address to all their borrow requests contract they made.
+   
     // Events
     event MultiSigCreated(
         address indexed user,
@@ -42,51 +48,66 @@ contract LimitMarket {
     );
 
     // Constructor
-    constructor() {}
+    constructor() {
+    }
 
     // Function to create a multisig contract between this contract and the caller, and transfer Ether to the newly created multisig
-    function createMultiSig(uint256 _collateral) external payable {
+    function createMultiSig(uint256 _collateral) external payable nonReentrant {
         // Check the amount to send is valid
         if (msg.value == 0) revert NoAmountSent(msg.sender.balance, msg.value);
         if (msg.value > msg.sender.balance) revert inValidAmount(msg.sender.balance, msg.value);
-        if (_collateral <= 0) revert NoCollateralSent(msg.value);
+        if (_collateral == 0) revert NoCollateralSent(msg.value);
         if (_collateral >= msg.sender.balance) revert InsufficientBalance(msg.sender.balance, _collateral);
          
-         address[2] memory owners=[msg.sender,address(this)];
-       
+        address[2] memory owners =[msg.sender,address(this)];
         CreateMultiSig multiSig = new CreateMultiSig(owners);
 
-        // Store the deployed MultiSig contract address
-        userToMultiSigs[msg.sender].push(address(multiSig));
-
-        // Store the multisig details in the struct
-        multiSigDetails[address(multiSig)] = MultiSigDetails({
-            depositedAmount: msg.value,
-            collateral: _collateral
-        });
-
-        // Store the borrow request for this multisig
-        borrowRequests[address(this)].push(address(multiSig));
-
-        // Transfer the Ether to the multisig contract
+          // withdraws memecoin amount
+        //  // Transfer the Ether to the multisig contract
         (bool success, ) = payable(address(multiSig)).call{
             value: msg.value, gas: 2300
         }("");
         if (!success) revert TransferFailed(address(multiSig), msg.value);
 
-        // Emit events
+
+        // // Emit events
         emit MultiSigCreated(msg.sender, address(multiSig), msg.value);
         emit MultiSigDetailsUpdated(address(multiSig), msg.value, _collateral);
+
+             totalBorrowersCount++;
+        //     // store values for borrow request parameters
+            userToBorrowRequestDetails[address(multiSig)] = borrowRequest({
+                depositedAmount: msg.value,
+                collateral: _collateral,
+                position: totalBorrowersCount
+            });
+
+        
+        protol_TotalActiveBorrowRequest_Array.push(address(multiSig));
+     userToBorrowRequestAddress[msg.sender].push(address(multiSig));
+            // userToBorrowRequestAddress[msg.sender].push(address(multiSig));
     }
 
-    // Function to retrieve all multisig contracts for a user
-    function getUserMultiSigs(address user)
+    // Function to retrieve all borrow request contracts for a user
+    function getUserToBorrowRequest_Addresses(address user)
         external
         view
         returns (address[] memory)
-    {
-        return userToMultiSigs[user];
+    {      
+        return userToBorrowRequestAddress[user];
     }
+
+    function getBorrowRequestDetails(address user) 
+    external 
+    view 
+    returns (uint256 depositedAmount, uint256 collateral) 
+{
+    borrowRequest memory request = userToBorrowRequestDetails[user];
+    return (request.depositedAmount, request.collateral);
+}
+
+
+
     // Function to check the contract's balance
     function getBalance() public view returns (uint256) {
      
@@ -94,13 +115,13 @@ contract LimitMarket {
     }
 
     // Function to retrieve details of a specific multisig
-    function getMultiSigDetails(address multiSig)
-        external
-        view
-        returns (MultiSigDetails memory)
-    {
-        return multiSigDetails[multiSig];
-    }
+    // function getBorrowRequest_Parameters(address borrowRequestAddress)
+    //     external
+    //     view
+    //     returns (address memory)
+    // {
+    //     return arrayOfAllBorrowRequests[borrowRequestAddress];
+    // }
      receive() external payable {
         // Ether is received and stored in the contract
     }
